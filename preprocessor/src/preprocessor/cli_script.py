c10cb3a13bb42d4f110440b4ef8d5d77f01cd0a2
jpwhite4 <jpwhite4@nairr-dev-01.ccr.xdmod.org>
2025-08-06 11:02:26 -0400

    Initial Commit

---
+++ preprocessor/src/preprocessor/cli_script.py
@@ -0,0 +1,11 @@
+"""
+DOCSTRING for first public console interface.
+
+USAGE:
+    xdmod-preprocess -c [PATH TO CONFIG FILE] -r [RESOURCE]
+"""
+
+
+def main():
+    """Print helloe sample."""
+    print('hello sample')  # noqa: T201
